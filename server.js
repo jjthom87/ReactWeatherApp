5c1f82687078951b8da4daa3db5e78748509ac19
Jared Thomas <jjthom87@yahoo.com>
2016-09-02 20:37:08 -0400

    works except for api

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ var app = express();
 
 app.use(express.static('public'));
 
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(function (req, res, next){
+	if (req.headers['x-forwarded-proto' === 'http']) {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+})
 
 app.listen(PORT, function(){
 	console.log('Express Server is up on port ' + PORT);
